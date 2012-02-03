f8e4ecd5a6c4bf5fc34bb7db23622034853ca140
mumoshu <ykuoka@gmail.com>
2012-02-04 00:43:25 +0900

    wtf should I add resolvers

--- project/Build.scala
+++ project/Build.scala
@@ -9,14 +9,13 @@ object ApplicationBuild extends Build {
 
     val furyu = Resolver.url("Furyu", url("http://furyu.github.com/repo/"))(Resolver.ivyStylePatterns)
 
-    resolvers += furyu
-
     val appDependencies = Seq(
       // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
+      resolvers += furyu
     )
 
 }
