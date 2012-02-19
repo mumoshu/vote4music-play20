cb56d19e9e65756aea1f8d4abd824efd6679960f
mumoshu <ykuoka@gmail.com>
2012-02-19 13:33:51 +0900

    Stripping the reference to the repository containing my own build of Play RC1.

--- project/Build.scala
+++ project/Build.scala
@@ -7,15 +7,11 @@ object ApplicationBuild extends Build {
     val appName         = "vote4music"
     val appVersion      = "1.0"
 
-    val furyu = Resolver.url("Furyu", url("http://furyu.github.com/repo/"))(Resolver.ivyStylePatterns)
-
     val appDependencies = Seq(
-      // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
-      resolvers += furyu
     )
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,8 +3,7 @@ logLevel := Level.Warn
 resolvers ++= Seq(
     DefaultMavenRepository,
     Resolver.url("Play", url("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns),
-    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-    Resolver.url("Furyu", url("http://furyu.github.com/repo/"))(Resolver.ivyStylePatterns)
+    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 )
 
 addSbtPlugin("play" % "sbt-plugin" % "2.0-RC1-SNAPSHOT")
